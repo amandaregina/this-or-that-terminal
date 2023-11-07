aa4ba714473c19648b31cbca2cff5a38c5ca892b
Amanda Regina Dos Santos Sales <amandaregina.s@hotmail.com>
2023-11-07 18:36:19 -0300

    Add project files.

---
+++ this-or-that/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ThisOrThat
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Isso:");
+            string thisValue = Console.ReadLine();
+
+            Console.WriteLine("Aquilo:");
+            string thatValue = Console.ReadLine();
+
+            string valueReturned = ReturnThisOrThat(thisValue, thatValue);
+
+            Console.WriteLine($"O escolhido foi: {valueReturned}");
+        }
+
+        static string ReturnThisOrThat(string value1, string value2)
+        {
+            Random random = new Random();
+            var thisOrThat = new List<string> { value1, value2 };
+            return (thisOrThat[random.Next(thisOrThat.Count)]);
+        }
+    }
+}
